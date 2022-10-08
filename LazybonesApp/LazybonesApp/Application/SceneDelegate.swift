e1da7fd8a9e73cd7708806a0194485e8f77b5d84
igor.dikan <igor.dikan@simbirsoft.com>
2022-10-08 10:59:21 +0300

    Commit text

---
+++ LazybonesApp/LazybonesApp/Application/SceneDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  SceneDelegate.swift
+//  LazybonesApp
+//
+//  Created by Anton Zyabkin on 05.10.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
+        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
+        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        setupRootViewController(windowScene: windowScene)
+    }
+    
+    private func setupRootViewController(windowScene: UIWindowScene) {
+        let window = UIWindow(windowScene: windowScene)
+        
+        // TODO: -
+        let viewController = MainViewController()
+        let networkService = NetworkService()
+        let presenter = MainPresenter(networkService: networkService)
+        
+        viewController.presenter = presenter
+        presenter.viewController = viewController
+        
+        window.rootViewController = UINavigationController(rootViewController: viewController)
+        
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+}
