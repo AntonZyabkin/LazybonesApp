d0366cf27745300d69e37ff99792897ab1ff4135
Anatan <zyabkinantonv@gmail.com>
2022-10-28 18:36:22 +0300

    Create ComingTableView+ComingCell to show Coming list

--- LazybonesApp/LazybonesApp/Core/ServiceLayer/APIService.swift
+++ LazybonesApp/LazybonesApp/Core/ServiceLayer/APIService.swift
@@ -9,10 +9,11 @@ import Foundation
 import Moya
 
 protocol APIServicable {
+    //TODO: должны ли мы все методы декларировать в одном протоколе
     func fetchPosts(complition: @escaping (Result<[Post], Error>) -> Void)
     func fetchComments(complition: @escaping (Result<[Comment], Error>) -> Void)
     func fetchUsers(complition: @escaping (Result<[User], Error>) -> Void)
-    func fetchComingList(complition: @escaping (Result<SbisComingListResponse, Error>) -> Void)
+    func fetchComingList(complition: @escaping (Result<SbisShortComingListResponse, Error>) -> Void)
     var target: TargetType { get }
 }
 
@@ -27,6 +28,7 @@ final class APIService{
 }
 
 extension APIService: APIServicable {
+    //MARK: - fetch jsonPlaceHolder Coments, Users, Posts
     func fetchPosts(complition: @escaping (Result<[Post], Error>) -> Void) {
         networkService.request(target, complition: complition)
     }
@@ -38,7 +40,13 @@ extension APIService: APIServicable {
     func fetchUsers(complition: @escaping (Result<[User], Error>) -> Void) {
         networkService.request(target, complition: complition)
     }
-    func fetchComingList(complition: @escaping (Result<SbisComingListResponse, Error>) -> Void) {
+    
+    //MARK: - fetch cominListDocuments
+    func fetchComingList(complition: @escaping (Result<SbisShortComingListResponse, Error>) -> Void) {
+        networkService.request(target, complition: complition)
+    }
+    
+    func sendAuthRequest(complition: @escaping (Result<SbisAuthResponse, Error>) -> Void) {
         networkService.request(target, complition: complition)
     }
 }
