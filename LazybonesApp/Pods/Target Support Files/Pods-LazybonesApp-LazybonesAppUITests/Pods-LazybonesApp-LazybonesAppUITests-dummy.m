d9238af00cd41c405288d933ba3755955c85c3ce
Anatan <zyabkinantonv@gmail.com>
2022-11-10 18:01:51 +0300

    delete Pods/ directory from index

--- LazybonesApp/Pods/Target Support Files/Pods-LazybonesApp-LazybonesAppUITests/Pods-LazybonesApp-LazybonesAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LazybonesApp_LazybonesAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LazybonesApp_LazybonesAppUITests
-@end
--- LazybonesApp/Pods/Target Support Files/Pods-LazybonesApp/Pods-LazybonesApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LazybonesApp : NSObject
-@end
-@implementation PodsDummy_Pods_LazybonesApp
-@end
